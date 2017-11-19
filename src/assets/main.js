39605b3e6a0358abf8ba2127f6d6c3ac67fdd8c4
Rafael Kinoshita <rafael.kinoshita@gmail.com>
2017-11-19 18:47:10 -0200

    changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
-
   // your code will go here
-
-});
+  $.ajax({
+    url: 'https://www.codeschool.com/users/rafael-kinoshita.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed );
+    }
+  });
+}); 
+function addCourses(courses){
+  var $badges = $('#badges');
+  courses.forEach(course => {
+    var $course = $('<div />', {'class':'course'}).appendTo(badges);
+    $('<h3 />', {text: course.title}).appendTo($course);
+    $('<img />', {src: course.badge}).appendTo($course);
+    $('<a />', {'class':'btn btn-primary', href: course.url, target:'_blank', text: 'See Course'}).appendTo($course);
+  })
+  
+}
